c19b3f9a43a5f790fd0d029fd0f1391a3c7e5534
Manish Kumar <kmanish75@gmail.com>
2020-01-15 09:24:46 -0700

    refactoring2

--- src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
+++ src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
@@ -1,24 +1,21 @@
 package io.pivotal.pal.tracker;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
-
     @Bean
-    TimeEntryRepository timeEntryRepository() {
-        MysqlDataSource mysqlDataSource = new MysqlDataSource();
-        mysqlDataSource.setURL(System.getenv("SPRING_DATASOURCE_URL"));
-        return new JdbcTimeEntryRepository(mysqlDataSource);
+    TimeEntryRepository timeEntryRepository(DataSource dataSource) {
+        return new JdbcTimeEntryRepository(dataSource);
     }
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
-
-
+    
 }
