b084a373a401f653d4912db5a8ead6710dadc2eb
Manish Kumar <kmanish75@gmail.com>
2020-01-15 09:01:15 -0700

    JDBC Template Assignment

--- src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
+++ src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.tracker;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,9 +8,12 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class PalTrackerApplication {
 
+
     @Bean
     TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+        MysqlDataSource mysqlDataSource = new MysqlDataSource();
+        mysqlDataSource.setURL(System.getenv("SPRING_DATASOURCE_URL"));
+        return new JdbcTimeEntryRepository(mysqlDataSource);
     }
 
     public static void main(String[] args) {
